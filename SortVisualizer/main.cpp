eeda01ff28cb25aceb050e178b8b0f07d61681cb
Yannick Felgitscher <yannick.felgitscher@gmail.com>
2017-11-10 08:10:00 +0100

    Assembly things

--- SortVisualizer/main.cpp
+++ SortVisualizer/main.cpp
@@ -1,4 +1,7 @@
 #include <GL\glew.h>
+#include <Windows.h>
+#include <iostream>
+
 #include "gradientvisualization.h"
 #include "renderer.h"
 #include "selectionsort.h"
@@ -10,13 +13,37 @@
 #include "quicksort.h"
 #include "mergesort.h"
 
+extern "C"
+{
+    void copyData(int* dst, int* src, int size);
+    void copyDataf(float* dst, float* src, int size);
+}
+
 int main(int argc, char* argv[])
 {
-	Visualization* renderer = new GradientVisualization(10000000);
+    // Test1: Integer test
+    int data1[4]{ 1, 2, 3, 4 };
+    int data2[4]{ 5, 6, 7, 8 };
+
+    copyData(data1, data2, 16);
+
+    std::cout << data1[0] << " " << data1[1] << " " << data1[2] << " " << data1[3] << std::endl;
+
+    // Test2: Float test
+    float dataf1[4]{ 1.1f, 1.2f, 1.3f, 1.4f };
+    float dataf2[4]{ 1.5f, 1.6f, 1.7f, 1.8f };
+
+    copyDataf(dataf1, dataf2, 4);
+
+    std::cout << dataf1[0] << " " << dataf1[1] << " " << dataf1[2] << " " << dataf1[3] << std::endl;
+
+    system("PAUSE");
+
+	/*Visualization* renderer = new GradientVisualization(10000000);
 	MergeSort sort;
 	renderer->init(0);
 	renderer->setAlgorithm(&sort);
 	renderer->loop();
 	delete renderer;
-	return 0;
+	return 0;*/
 }
