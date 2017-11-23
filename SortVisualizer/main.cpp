8d13167205ee6187b89db2cf62e21b21a8e44f0e
HOEGLER Stefan <hoestc13@htl-kaindorf.ac.at>
2017-11-23 13:08:36 +0100

    added dedicated transfer

--- SortVisualizer/main.cpp
+++ SortVisualizer/main.cpp
@@ -20,7 +20,7 @@
 int main(int argc, char* argv[])
 {
 
-	Visualization* renderer = new VulkanGradientVisualization(100000000);
+	Visualization* renderer = new VulkanGradientVisualization(10000000);
 	RapidQuickSort sort;
 	renderer->init(0);
 	renderer->setAlgorithm(&sort);
