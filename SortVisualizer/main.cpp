0447c1794dc28501eff89874d03790abc6cc6914
Yannick Felgitscher <yannick.felgitscher@gmail.com>
2017-11-23 19:57:05 +0100

    d

--- SortVisualizer/main.cpp
+++ SortVisualizer/main.cpp
@@ -17,15 +17,47 @@
 #include "quicksort.h"
 #include "mergesort.h"
 
+
+#define USE_ASSEMBLY 1
+
+extern "C"
+{
+    void asmswap(float* f1, float* f2);
+}
+
+
 int main(int argc, char* argv[])
 {
+    float f1 = 1.5f;
+    float f2 = 0.5f;
+    float helper;
 
-	Visualization* renderer = new VulkanGradientVisualization(100000000);
-	RapidQuickSort sort;
+    auto timepoint = std::chrono::high_resolution_clock::now();
+
+    for (unsigned int i = 0; i < 4294967294; i++)
+    {
+#if USE_ASSEMBLY == 1
+        asmswap(&f1, &f2);
+#else
+        helper = f1;
+        f1 = f2;
+        f2 = helper;
+#endif
+    }
+
+    long long duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - timepoint).count();
+    std::cout << f1 << " " << f2 << " it took " << duration << "mcs to complete" << std::endl;
+
+    system("PAUSE");
+
+	/*
+    Visualization* renderer = new GradientVisualization(100000);
+	BubbleSort sort;
 	renderer->init(0);
 	renderer->setAlgorithm(&sort);
 	renderer->loop();
 	delete renderer;
 	system("PAUSE");
 	return 0;
+    */
 }
